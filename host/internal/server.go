1cd4e330cc912bdf3de7cbd9a33b8f7a89ea7dc9
Owen Ou <jingweno@gmail.com>
2020-01-04 14:28:58 -0800

    Make host internals private

--- host/internal/server.go
+++ host/internal/server.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"os/exec"
 
-	"github.com/creack/pty"
 	gssh "github.com/gliderlabs/ssh"
 	"github.com/jingweno/upterm/upterm"
 
@@ -35,11 +34,11 @@ func (s *Server) ServeWithContext(ctx context.Context, l net.Listener) error {
 
 	emCtx, emCancel := context.WithCancel(ctx)
 	defer emCancel()
-	em := NewEventManager(emCtx, s.Logger.WithField("component", "event-manager"))
+	em := newEventManager(emCtx, s.Logger.WithField("component", "event-manager"))
 
 	cmdCtx, cmdCancel := context.WithCancel(ctx)
 	defer cmdCancel()
-	cmd := NewCommand(
+	cmd := newCommand(
 		s.Command[0],
 		s.Command[1:],
 		s.CommandEnv,
@@ -140,8 +139,8 @@ func (h *passwordHandler) HandlePassword(ctx gssh.Context, password string) bool
 
 type sessionHandler struct {
 	forceCommand []string
-	ptmx         *Pty
-	em           *EventManager
+	ptmx         *pty
+	em           *eventManager
 	writers      *uio.MultiWriter
 	ctx          context.Context
 	logger       log.FieldLogger
@@ -235,10 +234,10 @@ func (h *sessionHandler) HandleSession(sess gssh.Session) {
 	}
 }
 
-func startAttachCmd(ctx context.Context, c []string, term string) (*exec.Cmd, *Pty, error) {
+func startAttachCmd(ctx context.Context, c []string, term string) (*exec.Cmd, *pty, error) {
 	cmd := exec.CommandContext(ctx, c[0], c[1:]...)
 	cmd.Env = append(os.Environ(), fmt.Sprintf("TERM=%s", term))
-	pty, err := pty.Start(cmd)
+	pty, err := startPty(cmd)
 
-	return cmd, WrapPty(pty), err
+	return cmd, pty, err
 }
