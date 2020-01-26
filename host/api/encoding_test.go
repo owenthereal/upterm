45ba9fb835c4a0e046b5a6d2d6c8b493646f1c38
Owen Ou <jingweno@gmail.com>
2020-01-26 14:13:28 -0800

    Consider client version when routing host requests

--- host/api/encoding_test.go
+++ host/api/encoding_test.go
@@ -4,26 +4,53 @@ import (
 	"testing"
 
 	"github.com/golang/protobuf/proto"
+	"github.com/jingweno/upterm/upterm"
 )
 
 func Test_EncodeDecodeIdentifier(t *testing.T) {
-	want := &Identifier{
-		Id:       "owen",
-		Type:     Identifier_CLIENT,
-		NodeAddr: "127.0.0.1:22",
+	cases := []struct {
+		name          string
+		id            *Identifier
+		clientVersion string
+	}{
+		{
+			name: "client type",
+			id: &Identifier{
+				Id:       "client",
+				Type:     Identifier_CLIENT,
+				NodeAddr: "127.0.0.1:22",
+			},
+			clientVersion: "SSH-2.0-Go",
+		},
+		{
+			name: "host type",
+			id: &Identifier{
+				Id:   "host",
+				Type: Identifier_HOST,
+			},
+			clientVersion: upterm.HostSSHClientVersion,
+		},
 	}
 
-	str, err := EncodeIdentifier(want)
-	if err != nil {
-		t.Fatal(err)
-	}
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
 
-	got, err := DecodeIdentifier(str)
-	if err != nil {
-		t.Fatal(err)
-	}
+			want := c.id
+			str, err := EncodeIdentifier(want)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	if !proto.Equal(want, got) {
-		t.Errorf("Encode/decode failed, want=%s got=%s", want, got)
+			got, err := DecodeIdentifier(str, c.clientVersion)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if !proto.Equal(want, got) {
+				t.Errorf("Encode/decode failed, want=%s got=%s", want, got)
+			}
+		})
 	}
 }
