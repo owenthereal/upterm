590b1b16cf9246254b09b897ffbd8cf563a3023c
Owen Ou <jingweno@gmail.com>
2020-03-21 11:11:15 -0700

    Improve terraform script

--- terraform/heroku/main.tf
+++ terraform/heroku/main.tf
@@ -1,39 +1,40 @@
-variable "app_name" {
-  description = "Name of the Heroku app"
+variable "heroku_app_name" {
+  description = "Heroku app name"
 }
 
-variable "github_release" {
-  description = "GitHub release tag"
-  default = "master"
-}
-
-variable "region" {
+variable "heroku_region" {
   description = "Heroku region"
+  default = "us"
+}
+
+variable "heroku_space" {
+  description = "Name of the Heroku space"
   default = ""
 }
 
-variable "space" {
-  description = "Name of the Heroku space"
-  default = null
+variable "git_commit_sha" {
+  description = "Git commit sha on GitHub"
+  default = "master"
 }
 
-variable "organization" {
+variable "heroku_team" {
   description = "Heroku team"
   default = ""
 }
 
 resource "heroku_app" "uptermd" {
-  name   = var.app_name
-  region = var.region
+  name   = var.heroku_app_name
+  region = var.heroku_region
   buildpacks = [ "heroku/go" ]
-  space = var.space
+  space = var.heroku_space
+  acm = true
 
   sensitive_config_vars = {
     PRIVATE_KEY = "${tls_private_key.private_key.private_key_pem}"
   }
 
   organization {
-    name = var.organization
+    name = var.heroku_team
   }
 }
 
@@ -45,24 +46,24 @@ resource "tls_private_key" "private_key" {
 resource "heroku_app_feature" "spaces-dns-discovery" {
   app = heroku_app.uptermd.id
   name = "spaces-dns-discovery"
-  enabled = var.space == null ? false : true
+  enabled = var.heroku_space == "" ? false : true
 }
 
 resource "heroku_build" "uptermd" {
   app        = heroku_app.uptermd.id
 
   source = {
-    url     = "https://github.com/jingweno/upterm/archive/${var.github_release}.tar.gz"
-    version = var.github_release
+    url     = "https://github.com/jingweno/upterm/archive/${var.git_commit_sha}.tar.gz"
+    version = var.git_commit_sha
   }
 }
 
 resource "heroku_formation" "uptermd" {
   app        = heroku_app.uptermd.id
   type       = "web"
-  quantity   = var.space == null ? 1 : 2
-  size       = var.space == null ? "standard-1x" : "private-s"
-  depends_on = [ "heroku_build.uptermd" ]
+  quantity   = var.heroku_space == "" ? 1 : 2
+  size       = var.heroku_space == "" ? "standard-1x" : "private-s"
+  depends_on = [ heroku_build.uptermd ]
 }
 
 output "app_url" {
