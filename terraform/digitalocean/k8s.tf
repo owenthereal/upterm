303315ddfdbe6ba5f5737e2b09ccc497e8b6d60d
Owen Ou <o@owenou.com>
2020-10-03 17:37:11 -0700

    Don't load kubeconfig for helm

--- terraform/digitalocean/k8s.tf
+++ terraform/digitalocean/k8s.tf
@@ -67,6 +67,7 @@ locals {
 
 provider "helm" {
   kubernetes {
+    load_config_file       = false
     host                   = digitalocean_kubernetes_cluster.upterm.endpoint
     token                  = digitalocean_kubernetes_cluster.upterm.kube_config[0].token
     cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.upterm.kube_config[0].cluster_ca_certificate)
