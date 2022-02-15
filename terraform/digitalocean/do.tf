b2518910327ed3f98df62e506553ac36b36adeed
Owen Ou <o@owenou.com>
2022-02-15 11:26:10 -0800

    Bump Terraform deps

--- terraform/digitalocean/do.tf
+++ terraform/digitalocean/do.tf
@@ -69,4 +69,5 @@ resource "local_file" "kubeconfig" {
 output "kubeconfig" {
   depends_on = [digitalocean_kubernetes_cluster.upterm]
   value      = digitalocean_kubernetes_cluster.upterm.kube_config[0].raw_config
+  sensitive  = true
 }
--- terraform/digitalocean/versions.tf
+++ terraform/digitalocean/versions.tf
@@ -1,10 +1,12 @@
 terraform {
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
     }
     helm = {
-      source = "hashicorp/helm"
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
     }
   }
   required_version = ">= 0.13"
