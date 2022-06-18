8f8ffb3a94f0941ea170ad4db785739053bedfd2
corvofeng <corvofeng@gmail.com>
2022-06-18 16:39:30 +0800

    Close file handler as soon as we decompress it.
    Signed-off-by: corvofeng <corvofeng@gmail.com>

--- host/internal/vscodeweb.go
+++ host/internal/vscodeweb.go
@@ -73,12 +73,18 @@ func UntarGZ(tarball, target string) error {
 			continue
 		}
 
-		file, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, info.Mode())
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-		_, err = io.Copy(file, tarReader)
+		err = (func() error {
+			file, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, info.Mode())
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+			_, err = io.Copy(file, tarReader)
+			if err != nil {
+				return err
+			}
+			return nil
+		})()
 		if err != nil {
 			return err
 		}
