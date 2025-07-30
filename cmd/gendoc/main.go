64fea28b29d7265b4dea1f720b449cd5cfe3a878
Optimal One <o@optimalone.dev>
2025-07-29 21:04:15 -0700

    Remove SBOM generation and unused compression code
    - Remove SBOM configuration to simplify CI pipeline (syft not pre-installed)
    - Remove unused compressFile function from gendoc
    - Keep checksum signing for release integrity verification
    
    This streamlines the release process while maintaining security features.

--- cmd/gendoc/main.go
+++ cmd/gendoc/main.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"compress/gzip"
-	"io"
-	"os"
-
 	"github.com/owenthereal/upterm/cmd/upterm/command"
 	"github.com/owenthereal/upterm/internal/version"
 	log "github.com/sirupsen/logrus"
@@ -34,34 +30,4 @@ func main() {
 	if err := rootCmd.GenZshCompletionFile("./etc/completion/upterm.zsh_completion"); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func compressFile(filename string) error {
-	// Open the original file
-	file, err := os.Open(filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	// Create the compressed file
-	compressedFilename := filename + ".gz"
-	compressedFile, err := os.Create(compressedFilename)
-	if err != nil {
-		return err
-	}
-	defer compressedFile.Close()
-
-	// Create gzip writer
-	gzipWriter := gzip.NewWriter(compressedFile)
-	defer gzipWriter.Close()
-
-	// Copy file content to gzip writer
-	_, err = io.Copy(gzipWriter, file)
-	if err != nil {
-		return err
-	}
-
-	// Remove the original uncompressed file
-	return os.Remove(filename)
 }
