1e351fbbe3585f3325cba0a1da945e29fe3e2021
Max Schmitt <max@schmitt.mx>
2025-10-28 15:23:28 +0100

    Refactor admin client and server for unified Unix socket support; remove Windows-specific implementations.

--- host/adminclient.go
+++ host/adminclient.go
@@ -1,7 +1,9 @@
 package host
 
 import (
+	"context"
 	"fmt"
+	"net"
 
 	"github.com/owenthereal/upterm/host/api"
 	"google.golang.org/grpc"
@@ -17,13 +19,15 @@ func AdminSocketFile(sessionID string) string {
 }
 
 func AdminClient(socket string) (api.AdminServiceClient, error) {
-	target, err := getAdminTarget(socket)
-	if err != nil {
-		return nil, err
-	}
-
 	// Use mtls
-	conn, err := grpc.NewClient(target, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// Workaround for gRPC Unix socket support on Windows: https://github.com/grpc/grpc-go/issues/8675
+	conn, err := grpc.NewClient(
+		"passthrough:///unix",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithContextDialer(func(ctx context.Context, _ string) (net.Conn, error) {
+			return (&net.Dialer{}).DialContext(ctx, "unix", socket)
+		}),
+	)
 	if err != nil {
 		return nil, err
 	}
